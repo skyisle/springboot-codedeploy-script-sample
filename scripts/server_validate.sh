f04cdfb531e82289df455387e9df59f474676c48
Alan Jeon <ajeon@riotgames.com>
2017-04-25 01:03:37 +0900

    Add log

--- scripts/server_validate.sh
+++ scripts/server_validate.sh
@@ -5,5 +5,8 @@ do
     if [ "$(curl -s http://localhost:80/health)" = '{"status":"UP"}' ]
     then
         exit 0
+    else
+        echo "check server is running?"
+        sleep 3s
     fi
 done
